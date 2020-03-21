aa3bf7da5b517204654be10c981249df5f546fb9
LDirk <47995676+LDirk@users.noreply.github.com>
2020-03-21 10:39:47 -0300

    Add files via upload

---
+++ R-john hopkins/1- week/Cons_Input_Evaluation.R
@@ -0,0 +1,15 @@
+x = 1 
+print(x)
+# 1 
+
+msg = "Hello World"
+print(msg)
+# "Hello world" 
+
+x
+#Console, autoprint, 1 
+
+x = 1:20
+x 
+#[1]  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17
+#[18] 18 19 20
